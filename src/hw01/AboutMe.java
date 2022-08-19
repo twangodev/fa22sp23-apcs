d44b23bec7d3cc57310beaa03156a7a6619798a9
James Ding <48845764+twango-dev@users.noreply.github.com>
2022-08-18 20:07:20 -0700

    Initial commit with HW01

---
+++ src/hw01/AboutMe.java
@@ -0,0 +1,12 @@
+package hw01;
+
+public class AboutMe {
+
+    public static void main(String[] args) {
+        System.out.print("Hey I'm James. I enjoy coding, and I know Python, Java, Kotlin, and many other languages. ");
+        System.out.println("I'm also taking APCS this year, so that's pretty neat.\n");
+        System.out.print("I also think Java is pretty cool. I've been programming for a long time so this is all ");
+        System.out.println("pretty familiar :)");
+    }
+
+}
