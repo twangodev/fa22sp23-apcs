768df1c2efd790d94306b95dae8385533cf50fc0
James Ding <jamesding365@gmail.com>
2022-08-24 14:29:05 -0700

    Add classwork

---
+++ src/classwork/Boat.java
@@ -0,0 +1,17 @@
+package classwork;
+
+public class Boat {
+
+    public static void main(String[] args) {
+        String myBoat = ".  o .." + "\n" +
+                        "o . o o.o" + "\n" +
+                        "     ...oo" +"\n" +
+                        "      __[]__" +"\n" +
+                        "   __|_o_o_o\\__" + "\n" +
+                        "   \\\"\"\"\"\"\"\"\"\"\"\"/" + "\n" +
+                        "    \\.  ..  . /" + "\n" +
+                        "^^^^^^^^^^^^^^^^^^^";
+        System.out.println(myBoat);
+    }
+
+}
