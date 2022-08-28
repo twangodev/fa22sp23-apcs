a2b02fc771c7b3f98292cc304c29e23885c3ff05
James Ding <48845764+twango-dev@users.noreply.github.com>
2022-08-28 00:08:51 -0700

    Add MathHelper

---
+++ src/MathHelper.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MathHelper {
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        test(kb, "5 + 6", 11);
+        test(kb, "10 - 3", 7);
+    }
+
+    public static void test(Scanner kb, String str, int expected) {
+        System.out.print(str + " = ");
+        String response;
+        if (expected == kb.nextInt()) {
+            response = "Your answer is right";
+        } else {
+            response = "Your answer is wrong";
+        }
+        System.out.println(response);
+    }
+
+}
