c156f5ed4e8bcd479a70093b944cdf3feadf2280
James Ding <48845764+twango-dev@users.noreply.github.com>
2022-09-14 21:36:02 -0700

    Add CompoundInterest.java

---
+++ src/hw/CompoundInterest.java
@@ -0,0 +1,27 @@
+package hw;
+
+import java.util.Scanner;
+
+// James Ding
+public class CompoundInterest {
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        System.out.print("Enter the principle: ");
+        double principle = kb.nextDouble();
+        kb.nextLine();
+        System.out.print("Enter the interest rate in percent form: ");
+        double interest = kb.nextDouble() / 100;
+        double rate = 1 + interest;
+
+        int yearsPassed = 0;
+        double current = principle;
+        while (current <= principle * 2) {
+            current *= rate;
+            yearsPassed++;
+            System.out.println(yearsPassed + " year(s) has passed. You now have " + current);
+        }
+        System.out.println("It took " + yearsPassed + " for your principle to double to " + current);
+    }
+
+}
