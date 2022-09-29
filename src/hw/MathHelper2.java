e8bfc9c2f7451f6e1d6e21961e09f566ba3f1d69
James Ding <jamesding365@gmail.com>
2022-09-29 10:55:52 -0700

    Add javadocs for MathHelper2.java

--- src/hw/MathHelper2.java
+++ src/hw/MathHelper2.java
@@ -21,10 +21,21 @@ public class MathHelper2 {
         System.out.println("You got " + percentage + "% correct");
     }
 
+    /**
+     * Generate a pseudo random numbers within the range [a, b)
+     * @param a Lower limit for generation, inclusive
+     * @param b Upper limit for generation, exclusive
+     * @return A randomly generated number
+     */
     public static int rng(int a, int b) {
         return (int) (Math.random() * (b - a) + a);
     }
 
+    /**
+     * Converts an operation id to String
+     * @param id An integer from 0-3, representing the type of opereation
+     * @return The string representation of an operation id
+     */
     public static char getOperation(int id) {
         switch (id) {
             case 0: return '+';
@@ -35,6 +46,13 @@ public class MathHelper2 {
         }
     }
 
+    /**
+     * Produces the expected result of an operation
+     * @param id An integer from 0-3, representing the type of operation
+     * @param a The first input
+     * @param b The second input
+     * @return a id b
+     */
     public static int getExpectedResult(int id, int a, int b) {
         switch (id) {
             case 0: return a + b ;
@@ -45,6 +63,13 @@ public class MathHelper2 {
         }
     }
 
+    /**
+     * Test interaction with user
+     * @param kb Scanner
+     * @param str The question prompt
+     * @param expected Expected result
+     * @return Whether the user answered correctly
+     */
     public static boolean test(Scanner kb, String str, int expected) {
         System.out.print(str + " = ");
         String response;
@@ -55,6 +80,14 @@ public class MathHelper2 {
         return cond;
     }
 
+    /**
+     * Recursively run the game
+     * @param kb Scanner
+     * @param times Number of times to run game
+     * @param lowBound Lower RNG bound, inclusive
+     * @param highBound Upper RNG bound, exclusive
+     * @return The amount of correct answers the user scored
+     */
     public static int runGame(Scanner kb, int times, int lowBound, int highBound) {
         if (times < 1) return 0;
 
