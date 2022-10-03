1d1b8dbe62f682c0c389feca2f4b6cf269dbd5c7
James Ding <jamesding365@gmail.com>
2022-10-03 14:28:02 -0700

    Add Circle.java

---
+++ src/classwork/Circle.java
@@ -0,0 +1,27 @@
+package classwork;
+
+public class Circle {
+
+    private final double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    /**
+     * Calculates the circumference of a circle
+     * @return 2 * pi * radius
+     */
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    /**
+     * Calculates the area of a circle
+     * @return pi * radius ^ 2
+     */
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+}
