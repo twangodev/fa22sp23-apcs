92753d7757304410f27342074c5839b0003e8069
James Ding <jamesding365@gmail.com>
2022-10-03 09:37:02 -0700

    Add FRQ1.java

---
+++ src/quiz/FRQ1.java
@@ -0,0 +1,14 @@
+package quiz;
+
+public class FRQ1 {
+
+    static double findAverage(int n) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            int randomNumber = (int) (Math.random() * 91 + 10);
+            sum += randomNumber;
+        }
+        return (double) sum / n;
+    }
+
+}
