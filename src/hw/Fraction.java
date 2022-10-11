997d20545f13a4ba6951c8b757ac895f38cb3ffc
James Ding <48845764+twango-dev@users.noreply.github.com>
2022-10-10 19:21:03 -0700

    Added Fraction.java

---
+++ src/hw/Fraction.java
@@ -0,0 +1,41 @@
+package hw;
+
+public class Fraction {
+
+    private int numerator, denominator;
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
+    }
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public double eval() {
+        return (double) numerator / denominator;
+    }
+
+    public Fraction add(Fraction fraction) {
+        int num = numerator * fraction.denominator + denominator * fraction.numerator;
+        int gcd = denominator * fraction.denominator;
+        return new Fraction(num, gcd);
+    }
+
+    @Override
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
+
+    public static void main(String[] args) {
+        Fraction f = new Fraction(2, 5);
+        System.out.println(f.eval());
+        System.out.println(f.add(new Fraction(3, 7)));
+    }
+
+}
