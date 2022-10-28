3bc0d5a5d6dde2fb853092f3fbdbb5553249a6ac
James Ding <jamesding365@gmail.com>
2022-10-28 15:59:56 -0700

    Update Statistics.java

--- src/classwork/Statistics.java
+++ src/classwork/Statistics.java
@@ -34,19 +34,13 @@ public class Statistics {
     public int[] distribution() {
         int[] dist = new int[20];
 
-        for (int i = 0; i < ints.length; i++) {
-            int lowerBound = (i * 50) + 1;
-            int upperBound = lowerBound + 49;
-            for (int j : ints) {
-                if (j >= lowerBound && j <= upperBound) {
-                    dist[i]++;
-                }
-            }
+        for (int i : ints) {
+            dist[i / 50]++;
         }
         return dist;
     }
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
         Statistics stats = new Statistics();
         System.out.println("Average: " + stats.average());
         System.out.println("Largest: " + stats.largest());
