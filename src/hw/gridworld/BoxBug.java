5fe87f0af56b19aeaec9e629dea2ca01796e8243
James Ding <jamesding365@gmail.com>
2023-04-11 08:33:30 -0700

    Add BoxBug and SpiralBug

---
+++ src/hw/gridworld/BoxBug.java
@@ -0,0 +1,34 @@
+package hw.gridworld;
+
+import info.gridworld.actor.ActorWorld;
+import info.gridworld.actor.Bug;
+
+public class BoxBug extends Bug {
+
+    private int steps;
+    private int sideLength;
+
+    public BoxBug(int length) {
+        steps = 0;
+        sideLength = length;
+    }
+
+    public void act() {
+        if (steps < sideLength && canMove()) {
+            move();
+            steps++;
+        } else {
+            turn();
+            turn();
+            steps = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        ActorWorld world = new ActorWorld();
+        BoxBug bug = new BoxBug(3);
+        world.add(bug);
+        world.show();
+    }
+
+}
