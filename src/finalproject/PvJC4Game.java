3f38f3ea3eab577cb8f1dd3b4e5f51a97fccedea
James Ding <jamesding365@gmail.com>
2023-05-23 08:50:23 -0700

    Human player starts first

--- src/finalproject/PvJC4Game.java
+++ src/finalproject/PvJC4Game.java
@@ -6,8 +6,8 @@ public class PvJC4Game {
 
     public static void main(String[] args) {
         C4Board c4b = new C4Board();
-        C4Player p2 = new C4Human(Color.BLUE, Color.RED, "Player1", c4b);
-        C4Player p1 = new C4James(Color.RED, Color.BLUE, "James AI", c4b);
+        C4Player p1 = new C4Human(Color.BLUE, Color.RED, "Player1", c4b);
+        C4Player p2 = new C4James(Color.RED, Color.BLUE, "James AI", c4b, 5);
         c4b.initialize(p1, p2);
         c4b.show();
     }
